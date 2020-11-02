1bbceb6915fd8ca1bb0cb8fb002d2070d8d5c790
Camille Gabrieli <aziaziazi@gmail.com>
2020-11-02 18:16:31 +0100

    add routes and controllers functions

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+var User = require('../models/user');
+
+exports.index = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: index');
+}
+
+exports.user_create_get = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: user_create_get');
+}
+
+exports.user_create_post = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: user_create_post');
+}
+
+exports.user_delete_post = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: user_delete_post');
+}
+
+exports.user_update_post = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: user_update_post');
+}
+
+exports.user_detail_get = function(req, res, next) {
+  res.send('NOT IMPLEMENTED: user_detail_get');
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var user_controller = require('../controllers/userController');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', user_controller.index);
+
+router.get('/create', user_controller.user_create_get);
+router.post('/create', user_controller.user_create_post);
+
+router.post('/:id/delete', user_controller.user_delete_post);
+
+router.post('/:id/update', user_controller.user_update_post);
+
+router.get('/:id', user_controller.user_detail_get);
 
 module.exports = router;
